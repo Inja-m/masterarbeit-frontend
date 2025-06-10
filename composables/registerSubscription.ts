a6c9d7e6c822a2a1414c39d92c4ec4a3c484317b
Inja-m <inja@mainhardts.de>
2025-06-10 11:49:28 +0200

    only new subscription in default

--- composables/registerSubscription.ts
+++ composables/registerSubscription.ts
@@ -10,29 +10,31 @@ export const useRegisterSubscription = async () => {
 		toast.add({ title: 'Service Worker nicht unterstützt', color: 'error' })
     return
   }
+	if(Notification.permission === 'default'){
+		const permission = await Notification.requestPermission()
+		if (permission !== 'granted') {
+			console.warn('Benachrichtigungen nicht erlaubt.')
+			toast.add({
+				title: 'Benachrichtigungen deaktiviert',
+				description: 'Bitte erlaube Benachrichtigungen in deinen Browser-Einstellungen.',
+				color: 'warning',
+				icon: 'i-lucide-bell-off',
+			})
+			return
+		}
 
-  const permission = await Notification.requestPermission()
-  if (permission !== 'granted') {
-    console.warn('Benachrichtigungen nicht erlaubt.')
-		toast.add({
-      title: 'Benachrichtigungen deaktiviert',
-      description: 'Bitte erlaube Benachrichtigungen in deinen Browser-Einstellungen.',
-      color: 'warning',
-      icon: 'i-lucide-bell-off',
-    })
-    return
-  }
+		const registration = await navigator.serviceWorker.ready
+		const subscription = await registration.pushManager.subscribe({
+			userVisibleOnly: true,
+			applicationServerKey: urlBase64ToUint8Array(config.public.vapidPublicKey),
+		})
 
-  const registration = await navigator.serviceWorker.ready
-  const subscription = await registration.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(config.public.vapidPublicKey),
-  })
+		await client('/subscribe', {
+			method: 'POST',
+			body: subscription.toJSON(),
+		})
 
-  await client('/subscribe', {
-    method: 'POST',
-    body: subscription.toJSON(),
-  })
-
-  console.log('Push-Subscription erfolgreich registriert.')
+		console.log('Push-Subscription erfolgreich registriert.', permission)
+	}
+ 
 }
