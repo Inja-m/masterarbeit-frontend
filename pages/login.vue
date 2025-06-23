8049bff39b5d7258b6920e4f2df1cef6e8e3d18f
Inja-m <inja@mainhardts.de>
2025-06-23 18:12:41 +0200

    log

--- pages/login.vue
+++ pages/login.vue
@@ -39,6 +39,8 @@ const items = [
     slot: 'login' as const
   }
 ] satisfies TabsItem[]
-onMounted(async () => {if(user){await logout()}})
+onMounted(async () => {
+	if(user){await logout()}
+console.log(user)})
 	 
 </script>
