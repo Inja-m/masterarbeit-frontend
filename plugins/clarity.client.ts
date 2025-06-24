bf68b8304ebdb0909694f0f34313b4ce652ab7dd
Inja-m <inja@mainhardts.de>
2025-06-24 21:51:34 +0200

    troubleshouting clarity

--- plugins/clarity.client.ts
+++ plugins/clarity.client.ts
@@ -1,11 +1,19 @@
-export default defineNuxtPlugin(() => {
 
+
+export default defineNuxtPlugin(async () => {
+	const { fetchUser } = useStrapiAuth()
   const consent = useCookie('cookie-consent')
   const hasConsent = computed(() => consent.value === 'clarity-consent')
 
-  if (import.meta.env.MODE === 'production' && hasConsent.value) {
+  if (import.meta.env.MODE === 'production' && hasConsent.value) {	
+		if (!window.clarityInitialized) {
     const { proxy } = useScriptClarity({id: useRuntimeConfig().public.scripts.clarity.id})
+		const user = await fetchUser()
+		if(user.value) proxy.clarity('identify', user.value.documentId)
+		window.clarityInitialized = true
+    }
   } else {
     console.log('[Clarity Plugin] Clarity wird **nicht** geladen. ENV:', import.meta.env, 'Consent:', consent.value)
   }
+	
 })
