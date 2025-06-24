25edecfe050dcfa058c9acf08610c0585ee87de8
Inja-m <inja@mainhardts.de>
2025-06-24 22:17:21 +0200

    try

--- plugins/clarity.client.ts
+++ plugins/clarity.client.ts
@@ -1,21 +1,41 @@
 
 
-export default defineNuxtPlugin(async () => {
-	const { fetchUser } = useStrapiAuth()
-  const consent = useCookie('cookie-consent')
-  const hasConsent = computed(() => consent.value === 'clarity-consent')
+//export default defineNuxtPlugin(async () => {
+//	const { fetchUser } = useStrapiAuth()
+//  const consent = useCookie('cookie-consent')
+//  const hasConsent = computed(() => consent.value === 'clarity-consent')
 
-  if (import.meta.env.MODE === 'production' && hasConsent.value) {	
-		console.log('window',window.clarityInitialized)
-		if (!window.clarityInitialized) {
+//  if (import.meta.env.MODE === 'production' && hasConsent.value) {	
+//		console.log('window',window.clarityInitialized)
+//		if (!window.clarityInitialized) {
+//    const { proxy } = useScriptClarity({id: useRuntimeConfig().public.scripts.clarity.id})
+//		const user = await fetchUser()
+//		if(user?.value) proxy.clarity('identify', user?.value?.documentId)
+//			console.log('user',  user?.value?.documentId)
+//		window.clarityInitialized = true
+//    }
+//  } else {
+//    console.log('[Clarity Plugin] Clarity wird **nicht** geladen. ENV:', import.meta.env, 'Consent:', consent.value)
+//  }
+	
+//})
+
+export default () => {
+  const consent = useCookie('cookie-consent')
+	const hasConsent = computed(() => consent.value === 'clarity-consent')
+	const { fetchUser } = useStrapiAuth()
+
+  watch(consent, async (val) => {
+		console.log(val)
+     if (import.meta.env.MODE === 'production' && hasConsent.value) {	
+
     const { proxy } = useScriptClarity({id: useRuntimeConfig().public.scripts.clarity.id})
 		const user = await fetchUser()
 		if(user?.value) proxy.clarity('identify', user?.value?.documentId)
 			console.log('user',  user?.value?.documentId)
-		window.clarityInitialized = true
-    }
+    
   } else {
     console.log('[Clarity Plugin] Clarity wird **nicht** geladen. ENV:', import.meta.env, 'Consent:', consent.value)
   }
-	
-})
+  }, { immediate: true })
+}
