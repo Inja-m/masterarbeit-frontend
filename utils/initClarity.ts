3c565cca0a8bc7727d171c12c9af7a62fec33562
Inja-m <inja@mainhardts.de>
2025-06-26 15:02:10 +0200

    log user

--- utils/initClarity.ts
+++ utils/initClarity.ts
@@ -1,27 +1,23 @@
 
 export const initClarity = async () => {
 	if (import.meta.env.MODE !== 'production') return
-	const { fetchUser } = useStrapiAuth()
-	const user = await fetchUser()
+	const user = useStrapiUser()
  	const consent = useCookie('cookie-consent')
   const { proxy } = useScriptClarity({
     id: useRuntimeConfig().public.scripts.clarity.id
   })
-
+console.log(user)
 	watch(() => consent.value,
 				(newVal) => {
 					proxy.clarity('consent', computed(() => newVal === 'consent' ))
 				},
 				{ immediate: true }
 			)
-	watch(() => user.value,
-				(newVal) => {
-					if(newVal){
-						proxy.clarity('identify', newVal.documentId)
-						console.log('proxy', proxy)
-					}
-				},
-				{ immediate: true }
-			)		
+	watch(user, (newVal) => {
+		if (newVal) {
+			proxy.clarity('identify', newVal.documentId)
+			console.log('proxy', proxy)
+		}
+	}, { immediate: true })
 					
 }
