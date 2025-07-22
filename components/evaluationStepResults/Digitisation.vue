f9cf72b4e0eeba76900e87005050bfe71c1e66e1
Inja-m <inja@mainhardts.de>
2025-07-22 19:58:59 +0200

    dots carousel

--- components/evaluationStepResults/Digitisation.vue
+++ components/evaluationStepResults/Digitisation.vue
@@ -32,9 +32,9 @@
 					auto-height
           :items="carouselItems"
 					:ui="{
+						dot: 'w-3 h-1',
 						container: 'transition-[height]',
-						controls: 'absolute -top-8 inset-x-12',
-						dots: '-top-7',
+						controls: 'absolute -top-8 inset-x-2'
 					}"
           class="w-full max-w-xs mx-auto mb-6"
         >
--- components/evaluationStepResults/Qualitative.vue
+++ components/evaluationStepResults/Qualitative.vue
@@ -4,11 +4,14 @@
     v-slot="{ item }"
     :items="result"
     class="w-full max-w-2xl mx-auto my-4"
+		auto-height
     dots
     loop
-		:ui="{dot: 'bg-default'}"
+		:ui="{dot: 'bg-default w-2 h-1',
+		container: 'transition-[height]',
+		controls: 'absolute -top-8 inset-x-2',}"
   >
-    <UCard class="h-40 flex flex-col justify-between p-4 dark text-default text-sm">
+    <UCard class="flex flex-col justify-between p-4 dark text-default text-sm">
       <p>
         Als <strong>{{ item.role }}</strong> möchte ich <em>{{ item.goal }}</em
         >, damit {{ item.benefit }}.
